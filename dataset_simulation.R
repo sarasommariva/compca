93e0a9e26091c4d9905da1ffca53ff8f1575fa89
sarasommariva <sommariva@dima.unige.it>
2025-03-01 14:46:13 +0100

    Estimate p-value and first plain bootstrap

--- dataset_simulation.R
+++ dataset_simulation.R
@@ -10,35 +10,47 @@ library(utils)
 # Step 1. Definition of the parameters
 machine_toll = 5*.Machine$double.eps
 
-num_sim = 1000
+num_sim = 10
 D = 6
 n_y = 500
 n_z = n_y
 K = 2
 Q = 2
 
-#mu_y = rep(0, D-Q-1)
-#mu_z= rep(0, D-1)
-mu_y = c(20, 10, 3)
-mu_z = c(2, 1, 0, 10, 10)
+# Bootstrap parameters
+num_rip_boot = 10000
+
+mu_y = rep(0, D-Q-1)
+mu_z= rep(0, D-1)
+#mu_y = c(20, 10, 3)
+#mu_z = c(2, 1, 0, 10, 10)
 
 # Case 1. Diagonal matrices with structural zeros
 #omega_y = diag(c(10, 5, 3, 0, 0))
 #omega_z = diag(c(8, 7, 3, 2, 1))
 
 # Case 2. Non-diagonal matrices with structural zeros
-temp_covs = rand_covmat(D, K, Q, eigenval_y=c(10, 5, 3), 
-                        eigenval_z=c(8, 7, 3, 2, 1))
+#temp_covs = rand_covmat(D, K, Q, eigenval_y=c(10, 5, 3), 
+#                        eigenval_z=c(8, 7, 3, 2, 1))
 
 # Case 3. Matrices that do not satisfy H_0
-#temp_covs = rand_indep_covmat(D, K, Q, eigenval_y=c(10, 5, 3), 
-#                        eigenval_z=c(8, 7, 3, 2, 1))
+temp_covs = rand_indep_covmat(D, K, Q, eigenval_y=c(10, 5, 3), 
+                        eigenval_z=c(8, 7, 3, 2, 1))
 
 omega_y = temp_covs$omega_y
 omega_z = temp_covs$omega_z
 rm(temp_covs)
 
 stat_values = matrix(data=NA, nrow=num_sim, ncol=1)
+
+est_mu_T = matrix(data=NA, nrow=num_sim, ncol=1)
+est_sigma2_T = matrix(data=NA, nrow=num_sim, ncol=1)
+est_pvalues = matrix(data=NA, nrow=num_sim, ncol=1)
+
+boot_mu_T = matrix(data=NA, nrow=num_sim, ncol=1)
+boot_sigma2_T = matrix(data=NA, nrow=num_sim, ncol=1)
+boot_pvalues = matrix(data=NA, nrow=num_sim, ncol=1)
+
 
 for (isim in 1:num_sim){
   
@@ -90,14 +102,70 @@ for (isim in 1:num_sim){
   
   stat_values[isim] = sum(eigen_cov_y$values[1:K] + eigen_cov_z$values[1:K] - 
                    eigen_sum$values[1:K])
+  
+  # Step 7. Estimate the p-value
+  #   7.a. Estimate eigenvalues
+  est_alpha = c(eigen_cov_y$values/(n_y-1), matrix(0, Q, 1))
+  est_beta = eigen_cov_z$values/(n_z -1)
+  est_psi =  eigen_sum$values / (n_y + n_z - 2)
+  #   7.b. Estimate eigenvectors
+  est_mat_K = eigen_sum$vectors
+  temp1 = eigen(t(est_mat_K[,1:K]) %*% 
+                  adiag(est_cov_y, matrix(0, Q, Q)) %*% est_mat_K[,1:K])
+  temp2 = eigen(t(est_mat_K[,(K+1):(D-1)]) %*% 
+                  adiag(est_cov_y, matrix(0, Q, Q)) %*% est_mat_K[,(K+1):(D-1)])
+  est_mat_U = adiag(temp1$vectors, temp2$vectors); rm(temp1, temp2)
+  temp1 = eigen(t(est_mat_K[,1:K]) %*% est_cov_z %*% est_mat_K[,1:K])
+  temp2 = eigen(t(est_mat_K[,(K+1):(D-1)]) %*% est_cov_z %*% est_mat_K[,(K+1):(D-1)])
+  est_mat_V = adiag(temp1$vectors, temp2$vectors); rm(temp1, temp2)
+  #   7.c. Estimate parameters of the null distribution of the test statistic
+  est_paramsT = compute_param_nulldistr(est_alpha, est_beta, est_psi, 
+                                        est_mat_U, est_mat_V, n_y, n_z)
+  est_mu_T[isim] = est_paramsT$mu_T
+  est_sigma2_T[isim] = est_paramsT$sigma2_T
+  #   7.d. Compute p-value
+  est_const_chi = 0.5*est_sigma2_T[isim] / est_mu_T[isim]
+  est_df_chi = max(round(2*est_mu_T[isim]^2/est_sigma2_T[isim]), 1)
+  
+  est_pvalues[isim] = pchisq(stat_values[isim]/est_const_chi,
+                              est_df_chi, lower.tail = FALSE)
+  
+  # Step 8. Estimate p-value via bootstrap
+  #boot_stat_values = matrix(data=NA, nrow=num_rip_boot, ncol=1) 
+  # for (iboot in 1:num_rip_boot){
+  #   # 8.a.
+  #     Y_boot = Y_transf[randi(n_y, n_y, 1), ]
+  #     Z_boot = Z_transf[randi(n_z, n_z, 1), ]
+  #   
+  #   # 8.b 
+  #     boot_cov_y = crossprod(sweep(Y_boot, 2, colMeans(Y_boot)))
+  #     boot_cov_z = crossprod(sweep(Z_boot, 2, colMeans(Z_boot)))
+  #     boot_eigen_cov_y = eigen(boot_cov_y)
+  #     boot_eigen_cov_z = eigen(boot_cov_z)
+  #     boot_eigen_sum = eigen(adiag(boot_cov_y, matrix(0, Q, Q)) + boot_cov_z)
+  #     
+  #     boot_stat_values[iboot] = sum(boot_eigen_cov_y$values[1:K] 
+  #                   + boot_eigen_cov_z$values[1:K] - boot_eigen_sum$values[1:K])
+  # }
+  #   # 8.c
+  # boot_mu_T[isim] = mean(boot_stat_values)
+  # boot_sigma2_T[isim] = var(boot_stat_values)
+  # 
+  #   # 8.d
+  # boot_const_chi = 0.5*boot_sigma2_T[isim] / boot_mu_T[isim]
+  # boot_df_chi = round(2*boot_mu_T[isim]^2/boot_sigma2_T[isim])
+  # 
+  # boot_pvalues[isim] = pchisq(stat_values[isim]/boot_const_chi,
+  #                            boot_df_chi, lower.tail = FALSE)
 }
 
 hist(stat_values, 200)
 
 result = list('param' = list('n_y' = n_y, 'n_z' = n_z, 'K'=K, 'Q'=Q,
                           'omega_y' = omega_y, 'omega_z' = omega_z),
-                    'stat_values'=stat_values)
-save(result, file="prova_zeros_means.Rdata")
+                    'stat_values'=stat_values, 'est_mu_T'=est_mu_T,
+                    'est_sigma2_T'=est_sigma2_T, 'est_pvalues'=est_pvalues)
+save(result, file="prova_belinona.Rdata")
 
 #mu_stat_values = mean(stat_values)
 #var_stat_values = var(stat_values)
