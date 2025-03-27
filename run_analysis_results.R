74c6630622a7e2a942fa5f0fee5ff4512f420658
sarasommariva <sommariva@dima.unige.it>
2025-03-27 12:40:49 +0100

    Added some checks on Schott parameters

--- run_analysis_results.R
+++ run_analysis_results.R
@@ -16,7 +16,7 @@ alpha_sign = 0.05
 
 #   1.b. Load
 load(file.path(folder_res, 
-                paste0('res_K_', K_sim, '_distr_', distribution, '_2.Rdata')))
+                paste0('res_K_', K_sim, '_distr_', distribution, '.Rdata')))
 
 ny_all= result$param$ny_all  # Read from file!!!!
 nz_all = result$param$nz_all
@@ -45,9 +45,28 @@ for (iy in 1:length(ny_all)){
   }
 }
 
+# Step 2. Check on sign of mu_T and sigma2_T
+neg_stat_values = matrix(data=NA, nrow=length(nz_all), ncol=length(ny_all))
+neg_est_mu_T = matrix(data=NA, nrow=length(nz_all), ncol=length(ny_all))
+neg_est_sigma2_T = matrix(data=NA, nrow=length(nz_all), ncol=length(ny_all))
+neg_theo_mu_T = matrix(data=NA, nrow=length(nz_all), ncol=length(ny_all))
+neg_theo_sigma2_T = matrix(data=NA, nrow=length(nz_all), ncol=length(ny_all))
+
+for (iy in 1:length(ny_all)){
+  for (iz in 1:length(nz_all)){
+    aux_idx = length(nz_all)*(iy-1)+iz;
+    neg_stat_values[iz, iy] = sum(result$results[[aux_idx]]$stat_values<0)
+    neg_est_mu_T[iz, iy] = sum(result$results[[aux_idx]]$est_mu_T<0)
+    neg_est_sigma2_T[iz, iy] = sum(result$results[[aux_idx]]$est_sigma2_T<0)
+    neg_theo_mu_T[iz, iy] = sum(result$results[[aux_idx]]$theo_mu_T<0)
+    neg_theo_sigma2_T[iz, iy] = sum(result$results[[aux_idx]]$theo_sigma2_T<0)
+  }
+}
+
 print(power_fortex)
 
 # Some plots for start checking
+# P1. p-value as function of mu_T
 aux_ny = 100; aux_nz = 100;
 iy = match(aux_ny, ny_all); iz = match(aux_nz, nz_all)
 aux_ris = result$results[[length(nz_all)*(iy-1)+iz]]
@@ -55,14 +74,12 @@ aux_ris = result$results[[length(nz_all)*(iy-1)+iz]]
 plot(aux_ris$theo_mu_T, aux_ris$pvalues_schott_theo)
 title(result$results_name[[length(nz_all)*(iy-1)+iz]])
 
+# P3. p-value as function of df
 aux_ny = 100; aux_nz = 100;
 iy = match(aux_ny, ny_all); iz = match(aux_nz, nz_all)
-hist(result$results[[length(nz_all)*(iy-1)+iz]]$pvalues_schott_est, ylim=c(0, 120))
+aux_ris = result$results[[length(nz_all)*(iy-1)+iz]]
+df_est = pmax(round(2*aux_ris$est_mu_T^2/aux_ris$est_sigma2_T), 1)
+df_theo = pmax(round(2*aux_ris$theo_mu_T^2/aux_ris$theo_sigma2_T), 1)  
 
-aux_ny = 60; aux_nz = 60;
-iy = match(aux_ny, ny_all); iz = match(aux_nz, nz_all)
-hist(result$results[[length(nz_all)*(iy-1)+iz]]$pvalues_schott_est, ylim=c(0, 120))
-
-aux_ny = 20; aux_nz = 20;
-iy = match(aux_ny, ny_all); iz = match(aux_nz, nz_all)
-hist(result$results[[length(nz_all)*(iy-1)+iz]]$pvalues_schott_est, ylim=c(0, 120))
+plot(df_est, aux_ris$pvalues_schott_est)
+title(result$results_name[[length(nz_all)*(iy-1)+iz]])
